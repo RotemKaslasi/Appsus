959c2e28bad31f7573af25248cc92a3f09db7355
Or Bracha <orbracha2308@gmail.com>
2018-11-08 18:56:40 +0200

    css

--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -8,11 +8,15 @@ export default {
         <div class="horse-move">
             <div class="move-div">
                 <img class="run-horse" src="img/run.gif" alt="">
-                <span class="home-page-title">welcome</span>
+                <span class="home-page-title">welcome , choose your app</span>
             </div>
         </div>
-            <router-link tag="button" class="email-link" to="/email">Email App</router-link>
-            <router-link tag="button" class="keep-link" to="/keep">Keep App</router-link>
+            <router-link tag="button" class="email-link" to="/email">
+                <img src="img/email.png" alt="" srcset="">
+            </router-link>
+            <router-link tag="button" class="keep-link" to="/keep">
+                <img src="img/notes.png" alt="" srcset="">
+            </router-link>
     </section>
     `
 }
