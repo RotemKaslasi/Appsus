7e62b42648bc6c8af29381a752cd3d0d1401041c
rotemkaslsi <ksl.rotem@gmail.com>
2018-11-09 21:16:19 +0200

    about-page

--- js/pages/about-page.cmp.js
+++ js/pages/about-page.cmp.js
@@ -1,19 +1,28 @@
 export default {
-    template:`
+    template: `
     <section>
-        <h1>ABOUT US</h1>
-            <div class="or-container">
+        <h1 class ="about-title">ABOUT US</h1>
+        <div  class="about-container">
+            <div class="staff-container">
+                    <h3 class="about-name">Or Bracha</h3>
+                    <img class="img-id" id="or-img" src="../img/or-img2.jpg">
+                    <p> text text text text text text text text text text 
+                    text text text text text text text text text text text 
+                    </p> 
+                </div>
 
-            </div>
+                <div class="staff-container">
+                <h3 class="about-name">Rotem Kaslasi</h3>   
+                    <img class="img-id" id="rotem-img" src ="../img/rotem-img.jpg">
+                    <p> 26 years old, from Ashdod.<br>
+                        My favorite Disney movies are "The Lion-King" and "Frozen".
+                        <br>My motto is: 
+                        "When things gets hard, eat cakes"
+                    </p>
+                </div>
 
+            </div>    
 
-
-
-
-
-             <div class="rotem-container">
-
-            </div>
 
 
 
--- js/routes.js
+++ js/routes.js
@@ -1,5 +1,6 @@
 'use strict'
 import homePage from './pages/home-page.cmp.js'
+import aboutPage from './pages/about-page.cmp.js'
 import emailApp from './email/cmps/email-app.cmp.js'
 import emailDetails from './email/cmps/email-details.cmp.js'
 import keepApp from './miss-keep/cmps/keep-app.cmp.js'
@@ -10,6 +11,7 @@ import noteEdit from './miss-keep/cmps/note-edit.cmp.js'
 
 const routes = [
     { path: '/', component: homePage },
+    { path: '/about', component: aboutPage },
     { path: '/email', component: emailApp },
     { path: '/email/:emailId', component: emailDetails },
     { path: '/keep', component: keepApp },
