410097394464f87439a45ac085cb94c8981b526c
Or Bracha <orbracha2308@gmail.com>
2018-11-09 21:58:48 +0200

    css

--- js/pages/about-page.cmp.js
+++ js/pages/about-page.cmp.js
@@ -2,13 +2,21 @@ export default {
     template: `
     <section>
         <h1 class ="about-title">ABOUT US</h1>
+         <div class="horse-move">
+            <div class="move-div">
+                <img class="run-horse" src="img/run.gif" alt="">
+                <span class="home-page-title">hey there,this is who we are!</span>
+            </div>
+        </div> 
         <div  class="about-container">
             <div class="staff-container">
                     <h3 class="about-name">Or Bracha</h3>
                     <img class="img-id" id="or-img" src="../img/or-img2.jpg">
-                    <p> text text text text text text text text text text 
-                    text text text text text text text text text text text 
-                    </p> 
+                    <p> 23 years old, from Kfar-Yona.<br>
+                        My favorite Disney movies are "The Lion-King" and "Aladin".
+                        <br>My motto is: 
+                        "Go Hard Or Go Home!"
+                    </p>
                 </div>
 
                 <div class="staff-container">
@@ -22,10 +30,6 @@ export default {
                 </div>
 
             </div>    
-
-
-
-
     </section>
    
    
--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -4,13 +4,6 @@
 export default {
     template:`
     <section class="home-page-container">
-        <!-- <h1>Welcome!</h1> -->
-        <!-- <div class="horse-move">
-            <div class="move-div">
-                <img class="run-horse" src="img/run.gif" alt="">
-                <span class="home-page-title">welcome , choose your app</span>
-            </div>
-        </div> -->
             <router-link tag="button" class="email-link" to="/email">
                 <img src="img/email.png" alt="" srcset="">
             </router-link>
