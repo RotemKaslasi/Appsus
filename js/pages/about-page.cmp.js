ef3f1ea1963cd8d256c488f75221e066b0f6b155
rotemkaslsi <ksl.rotem@gmail.com>
2018-11-09 21:31:16 +0200

    fix img

--- js/pages/about-page.cmp.js
+++ js/pages/about-page.cmp.js
@@ -5,7 +5,7 @@ export default {
         <div  class="about-container">
             <div class="staff-container">
                     <h3 class="about-name">Or Bracha</h3>
-                    <img class="img-id" id="or-img" src="../img/or-img2.jpg">
+                    <img class="img-id" id="or-img" src="./img/or-img2.jpg">
                     <p> text text text text text text text text text text 
                     text text text text text text text text text text text 
                     </p> 
@@ -13,7 +13,7 @@ export default {
 
                 <div class="staff-container">
                 <h3 class="about-name">Rotem Kaslasi</h3>   
-                    <img class="img-id" id="rotem-img" src ="../img/rotem-img.jpg">
+                    <img class="img-id" id="rotem-img" src ="./img/rotem-img.jpg">
                     <p> 26 years old, from Ashdod.<br>
                         My favorite Disney movies are "The Lion-King" and "Frozen".
                         <br>My motto is: 
